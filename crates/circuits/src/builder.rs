915c2192a1a8c89369843310e94bd84ae71ff9b5
rubydusa <rubydusa@protonmail.com>
2026-01-11 12:39:27 +0200

    Revert "refactor: accept iterator"
    This reverts commit 1500c44b7dacbf32d55da020e8328b5f55f8cf0d.

--- crates/circuits/src/builder.rs
+++ crates/circuits/src/builder.rs
@@ -119,31 +119,33 @@ impl CircuitBuilder {
     /// # Returns
     ///
     /// The outputs of the gate.
-    pub fn add_xor_gates<I>(&mut self, xy: I) -> Vec<Node<Feed>> 
-    where I: Iterator<Item = (Node<Feed>, Node<Feed>)>{
+    pub fn add_xor_gates(&mut self, xy: &[(Node<Feed>, Node<Feed>)]) -> Vec<Node<Feed>> {
         // if either input is a constant, we can simplify the gate
-        xy.map(|(x, y)|
-            if x.id() == y.id() {
+        let mut nodes = Vec::with_capacity(xy.len());
+        for (x, y) in xy {
+            let out = if x.id() == y.id() {
                 self.get_const_zero()
             } else if x.id() == 0 {
-                y
+                *y
             } else if y.id() == 0 {
-                x
+                *x
             } else if x.id() == 1 {
                 let out = self.add_feed();
-                self.gates.push(Gate::Inv { x: y.into(), z: out });
+                self.gates.push(Gate::Inv { x: (*y).into(), z: out });
                 out
             } else if y.id() == 1 {
                 let out = self.add_feed();
-                self.gates.push(Gate::Inv { x: x.into(), z: out });
+                self.gates.push(Gate::Inv { x: (*x).into(), z: out });
                 out
             } else {
                 let out = self.add_feed();
                 self.xor_count += 1;
-                self.gates.push(Gate::Xor { x: x.into(), y: y.into(), z: out });
+                self.gates.push(Gate::Xor { x: (*x).into(), y: (*y).into(), z: out });
                 out
-            }
-        ).collect()
+            };
+            nodes.push(out);
+        }
+        nodes
     }
 
     /// Adds an AND gate to the circuit.
